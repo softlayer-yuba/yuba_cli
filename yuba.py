a184d4c5176b75b662d3cd926ec5922fb4b7a7f6
hssh <hssh@hssh.jp>
2015-03-21 16:40:08 +0900

    VSManagerの生成およびverify_create_instanceによる生成テストコード実装

--- yuba.py
+++ yuba.py
@@ -1,8 +1,20 @@
 import click
+import SoftLayer
 import yaml
+
+from pprint import pprint
 
 @click.command()
 @click.argument('setting', type=click.File('r'), required=True)
 def cli(setting):
     params = yaml.load(setting)
     print params
+
+    client = SoftLayer.Client()
+
+    vsi = SoftLayer.VSManager(client)
+    result = vsi.verify_create_instance(**params)
+    pprint(result)
+
+    # result = vsi.create_instance(**params)
+    # pprint(result)
