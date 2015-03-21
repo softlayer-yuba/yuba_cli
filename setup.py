4fee39f928dc05a93ce7e44378bc87cb92a3e24f
ShimomuraYoshio <shimomura@Halley.local>
2015-03-21 15:20:46 +0900

    hello worldコマンド実装

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='yuba_cli',
+    version='0.1.0',
+    py_modules=['yuba'],
+    install_requires = [
+        'Click',
+    ],
+    entry_points = '''
+        [console_scripts]
+        yuba=yuba:cli
+    '''
+)
