0db9abf798b9ca923cbfc9351a5e6177e48b96f8
maisuto <shimomura@usa-mimi.jp>
2015-05-23 18:38:17 +0900

    #5 テスト作成中

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .sample import *
+from .cost import *
---
+++ tests/cost.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+
+from SoftLayer import testing
+
+import yuba
+
+
+class CostTest(testing.TestCase):
+
+    def test_cost(self):
+        pass
+
+
